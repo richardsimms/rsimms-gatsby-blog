1ed40ab5d57c7eba42b90b9e6a05e721cd523a75
Richard Simms <richardsimms@users.noreply.github.com>
2021-07-09 22:14:55 +1000

    update config website.js

--- config/website.js
+++ config/website.js
@@ -5,8 +5,8 @@ module.exports = {
   siteTitleShort: 'Richard', // short_name for manifest
   siteUrl: 'https://rsimms.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
-  siteDescription: 'Richard Simms, a Design Lead. Over the last 15 years, I have designed and developed projects in the digital space, pretty much anything that has a screen.',
+  siteLogo: 'images/thumbnail.jpg', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: 'A Design Lead with over the last 15 years, Richard has designed and developed projects in the digital space, pretty much anything that has a screen.',
   author: 'Richard Simms', // Author for schemaORGJSONLD
   organization: 'rsimms',
 
@@ -14,7 +14,6 @@ module.exports = {
   userTwitter: '@_rsimms', // Twitter Username
   ogSiteName: '', // Facebook Site Name
   ogLanguage: 'en_US',
-  ogImage: 'image/thumbnail.jpg', // image 
   googleAnalyticsID: 'UA-56512737-4',
 
   // Manifest and Progress color
