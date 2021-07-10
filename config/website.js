cececbc3f7c1160a5707b989678d9a1d482b7179
Richard Simms <richardsimms@users.noreply.github.com>
2021-07-10 18:52:17 +1000

    website.js config

--- config/website.js
+++ config/website.js
@@ -2,10 +2,10 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
   siteTitle: 'Richard Simms a UX design lead with over 15 years experience', // Navigation and Site Title
   siteTitleAlt: 'The work and writing of Richard Simms', // Alternative Site title for SEO
-  siteTitleShort: 'Richard', // short_name for manifest
+  siteTitleShort: 'Richard Simms', // short_name for manifest
   siteUrl: 'https://rsimms.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/images/thumbnail.jpg', // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteLogo: '/thumbnail.jpg', // Used for SEO and manifest, path to your image you placed in the 'static' folder
   siteDescription: 'As a design leader Richard has created products for global brands. Optimising user experiences and strategies to create products that deliver business growth.',
   author: 'Richard Simms', // Author for schemaORGJSONLD
   organization: 'rsimms',
