db10e472a80b78492b8df3bd16c2a0dd317e1d1e
Richard Simms <richardsimms@users.noreply.github.com>
2021-07-10 18:42:12 +1000

    thumbnail.jpg moved

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
     description: config.siteDescription,
     keywords: ['UX designer'],
     canonicalUrl: config.siteUrl,
-    image: "/images/thumbnail.jpg",
+    image: "/thumbnail.jpg",
     author: {
       name: config.author,
       minibio: `
