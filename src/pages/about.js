c3c92228683bd4c13327e1047374f47f0eba69df
Richard Simms <richardsimms@users.noreply.github.com>
2021-08-22 18:39:56 +1000

    updated new position

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,16 +13,16 @@ export default ({ data: { site } }) => {
             <img className="Image" src={require('../images/richard.webp')} alt="Richard Simms" />
             <h1>About Richard</h1>
            <p>Hi! I’m Richard Simms a user experience designer, I qualified from a masters in computer animation in 2005 and since then I’ve been designing for broadcast, websites and mobile apps. I’m obsessed about crafting amazing digital products. Through design rigger, I increase confidence in assumptions to create better experiences people are willing to change their behaviour for.</p>
-           <p>I’ve had the pleasure to work with <b>Diageo</b>, <b>Sky Sports</b>, <b>Coutts bank</b>, <b>Suncorp</b>, <b>MasterCard</b>, and many more household name brands. I'm currently crafting experiences at <b>Open Universities Australia</b>.</p>
+           <p>I’ve had the pleasure to work with <b>Diageo</b>, <b>Sky Sports</b>, <b>Coutts bank</b>, <b>Suncorp</b>, <b>MasterCard</b>, and many more household name brands. I'm currently crafting experiences at <b>SEEK Learning</b>.</p>
 
             <p>If you have additional questions, you can <a href="mailto:&#x72;&#x69;&#x63;&#x68;&#x61;&#x72;&#x64;@&#x72;&#x73;&#x69;&#x6d;&#x6d;&#x73;.&#x63;&#x6f;&#x6d;">email me</a> directly.</p>
             <p>When I’m not in front of a screen, you can find <a aria-label="View my strava bio" href="https://www.strava.com/athletes/12283677">me on a bike</a>, riding through the Australian bush or at least finding my Zen on Beach Road, Melbourne, AU.</p>
             <hr />
             <h2>Happening now</h2>
-            <p>As of May 2021:</p>
+            <p>As of August 2021:</p>
             <ul>
-              <li>Launching a self-service product for  University partners to manage their students.</li>
-              <li>Reading: <a target="_blank" rel="noopener noreferrer" aria-label="Working Backwards detail page on Amazon" href="https://www.amazon.com/gp/product/1250267595/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=1250267595&linkCode=as2&tag=rsimms-20&linkId=0d200c54af376416d7135e5b4fc70300">Working Backwards: Insights, Stories, and Secrets from Inside Amazon</a>.</li>
+              <li>Just started a new role at SEEK and said goodbye to friends and colleagues at Open Universities Australia.</li>
+              <li>Reading: Continuous Discovery Habits</li>
               <li>Writing about <a aria-label="View article on value-based design" href="https://vbd.substack.com/">value-based design</a> practice. Connecting the dots between customer value and business outcomes.</li>
             </ul>
             <hr />
